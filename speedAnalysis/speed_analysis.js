65332c35c19eee77d7349ffcd4abaf5eb2bea12e
Max <maxwell.prososki@abbott.com>
2025-07-06 20:00:33 -0400

    speed analysis task

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -36,9 +36,12 @@ button.onclick = endTest;
                 wpm = Math.round((typedWords / timeElapsed) * 60);
             }
 
+            var totallength = userTypedText.length
+
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+                "<p>Total Length: " + totallength + "</p>" +
                 "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
